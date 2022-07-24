3724bbb7b5fc43ad8a8274f48ecfba5ea9e9897c
Tabrej <tabrejhussain7@gmail.com>
2022-07-24 16:10:05 +0530

    -acvbnm

--- objects_ex.js
+++ objects_ex.js
@@ -6,9 +6,9 @@
 // }
 
 
-emp={
-    name:"Innaya",
-    gender:'F'
+emp = {
+    name: "Innaya",
+    gender: 'F'
 };
 
 // console.log(emp);
@@ -40,3 +40,73 @@ let employee = {
 
 console.log('ssn' in employee);
 console.log('employeeId' in employee);
+
+
+
+//Object Property types
+
+/*Objects have two types of properties: data and accessor properties.
+
+1) Data properties
+
+A data property contains a single location for a data value. A data property has four attributes:
+
+ [[Configurarable]] – determines whether a property can be redefined or removed via delete operator.
+ [[Enumerable]] – indicates if a property can be returned in the for...in loop.
+ [[Writable]] – specifies that the value of a property can be changed.
+ [[Value]] – contains the actual value of a property.
+By default, the [[Configurable]] , [[Enumerable]]And [[Writable]] attributes set to true for all properties defined directly on an object. The default value of the[[Value]] attribute is undefined.
+
+*/
+
+let person = {
+    firstName: 'John',
+    lastName: 'Doe'
+};
+
+
+/*
+
+To change any attribute of a property, you use the Object.defineProperty() method.
+
+The Object.defineProperty() method accepts three arguments:
+
+An object.
+A property name of the object.
+A property descriptor object that has four properties: configurable, enumerable, writable, and value.
+
+*/
+
+let person1 = {};
+person1.age = 25;
+console.log("Before", person1.age);
+
+delete person1.age;
+
+console.log("After", person1.age);
+
+// The following example creates a person object and adds the ssn property to it using the Object.defineProperty() method:
+
+// 'use strict';
+
+let person2 = {};
+
+Object.defineProperty(person2, 'ssn', {
+    configurable: false,
+    value: '012-38-9119'
+});
+console.log(person2.ssn);
+
+
+/*By default, the enumerable attribute of all the properties defined on an object is true. t means that you can iterate over all object properties using the for...in loop like this:
+
+
+
+*/
+let person3 = {};
+person3.age = 25;
+person3.ssn = '012-38-9119';
+
+for (let property in person3) {
+    console.log(property);
+}
